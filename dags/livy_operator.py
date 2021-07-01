ea89f0ca07fe54d0cfb53492a5b5431812bb9141
Mikaela Pisani <mikaela.pisani@rootstrap.com>
2021-07-01 14:27:19 -0300

    fix taskid parameter

--- dags/livy_operator.py
+++ dags/livy_operator.py
@@ -27,7 +27,7 @@ dag = DAG("livy-test", default_args=default_args,schedule_interval= '@once')
 t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 
 spark_task = LivyOperator(
-    rask_id='spark_task',
+    task_id='spark_task',
     file='local:///opt/spark/examples/src/main/python/pi.py', 
     class_name='org.apache.spark.examples.SparkPi', 
     args=[10], 
