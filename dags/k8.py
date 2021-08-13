5631d4151376c3b5614e2633bb4b5fa8ef087cdb
Anthony Figueroa <anthony@Anthonys-MacBook-Air.local>
2021-08-13 14:41:05 -0300

    k8

--- dags/k8.py
+++ dags/k8.py
@@ -21,12 +21,37 @@ default_args = {
 
 dag = DAG("kubernetes-test", default_args=default_args,schedule_interval= '@once')
 
+self.volume_mount = VolumeMount('input-data',
+                                mount_path='/data',
+                                sub_path=None,
+                                read_only=False)
+
+volume_config = {
+    'persistentVolumeClaim':
+        {
+            'claimName': 'pv-claim'
+        }
+}
+volume = Volume(name='test', configs=volume_config)
+input_volume = VolumeMount('input',
+    mount_path='/input',
+    sub_path=None,
+    read_only=True)
+
+#output_volume = VolumeMount('output',
+#    mount_path='/output',
+#    sub_path=None,
+#    read_only=False)
+
+
 t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 kubernetes_min_pod = KubernetesPodOperator(
-    task_id='pod-ex-minimum',
-    name='pod-ex-minimum',
+    task_id='pod-ex-kub',
+    name='pod-ex-kub',
     cmds=['echo'],
     namespace='airflow',
+    volume=[volume],
+    volume_mount=[input_volume],
     image='rootstrap/docker-airflow-ctakes', dag=dag)
 
 
