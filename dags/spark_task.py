a0497be8a93c43cb3547295cee31cc71b11af396
Mikaela Pisani <mikaela.pisani@rootstrap.com>
2021-08-19 18:17:04 -0300

    remove polling interval

--- dags/spark_task.py
+++ dags/spark_task.py
@@ -29,7 +29,6 @@ t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
 spark_task = LivyOperator(
     task_id='spark_task',
     file='local:///opt/spark/work-dir/uml_concepts.py', 
-    polling_interval=5,
     name="task-" + str(datetime.today().strftime('%Y%m%d%H%M%S')),
     conf={
             "spark.kubernetes.driver.pod.name" : "spark-pi-driver-" + str(datetime.today().strftime('%Y%m%d%H%M%S')),
